7e5dfb5dfd3cd2f5639759408e0031b741f81ba7
sweettry <105031615+sweettry@users.noreply.github.com>
2024-12-22 18:06:31 -0600

    Boiler Plate_v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Geist, Geist_Mono } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
